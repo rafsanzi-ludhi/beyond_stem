b3abbb531a9ab2708e053f0b0f813bc6e1f23ae1
Khavan <krajput123@hotmail.com>
2025-04-22 12:30:05 +0100

    getting inputs from DOM

--- client/js/common.js
+++ client/js/common.js
@@ -1,7 +1,4 @@
-const firstName = document.querySelector('#first-name')
-const lastName = document.querySelector('#last-name')
-const email = document.querySelector('#email-add')
-const password = document.querySelector('pwd')
-
-console.log(firstName, lastName, email, password)
-
+function getInputs(){
+    const email = document.getElementById('email').value
+    const password = document.getElementById('pwd').value
+}
