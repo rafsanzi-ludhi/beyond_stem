53fbf7a8ce2bba305e95feb467eb5e25be99218f
Khavan <krajput123@hotmail.com>
2025-04-22 12:18:08 +0100

    commit changes

--- client/js/common.js
+++ client/js/common.js
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 const firstName = document.querySelector('#first-name')
 const lastName = document.querySelector('#last-name')
 const email = document.querySelector('#email-add')
 const password = document.querySelector('pwd')
 
 console.log(firstName, lastName, email, password)
-=======
 
->>>>>>> 3843435d9bffeafc9903acc9e4e553450ed5572b
