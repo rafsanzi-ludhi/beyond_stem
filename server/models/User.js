21c0b5e31e6b9bc1152bdaee5d22cd7519d934de
bobby2910 <b.mastacan@gmail.com>
2025-04-23 07:28:57 +0100

    Fix authentication

--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,10 @@
 const db = require("../db/connect");
 
 class User {
-  constructor({ user_id, username, password, is_admin }) {
+  constructor({ user_id, username, password }) {
     this.id = user_id;
     this.username = username;
     this.password = password;
-    this.isAdmin = is_admin;
   }
 
   static async getOneById(id) {
@@ -31,7 +30,7 @@ class User {
   }
 
   static async create(data) {
-    const { username, password, isAdmin } = data;
+    const { username, password } = data;
     let response = await db.query(
       "INSERT INTO user_account (username, password) VALUES ($1, $2) RETURNING user_id;",
       [username, password]
