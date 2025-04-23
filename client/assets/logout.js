d1807cec2dd8bfec2d1f569ed7ef96ec06553bd5
bobby2910 <b.mastacan@gmail.com>
2025-04-23 09:58:49 +0100

    Add logout functionality, add footer on cardPage

--- client/assets/logout.js
+++ client/assets/logout.js
@@ -1,4 +1,5 @@
-document.getElementById("logout").addEventListener("click", () => {
+document.querySelector(".logout-btn").addEventListener("click", () => {
+  console.log(123);
   localStorage.removeItem("token");
   window.location.assign("./index.html");
 });
