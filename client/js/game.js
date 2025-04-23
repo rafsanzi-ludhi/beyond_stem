2e437d74db751e1b1eccfdf801e7f4307f179607
Khavan <krajput123@hotmail.com>
2025-04-23 12:34:41 +0100

    fixed image not appearing

--- client/js/game.js
+++ client/js/game.js
@@ -12,7 +12,8 @@ function displayFact(index) {
   card.style.maxWidth = "600px";
 
   const img = document.createElement("img");
-  img.src = `http://localhost:3000/${data.fact_img}`; // What is the image path???!! :(((((
+  img.src = `${data.fact_img}`; // What is the image path???!! :(((((
+  console.log(data.fact_img);
   img.alt = "Fact image";
   img.className = "card-img-top";
   card.appendChild(img);
